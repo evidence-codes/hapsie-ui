776ba633f0404613b9298bbfcd629bba3ee9dee4
evidence-codes <adejuwonevidence181@gmail.com>
2024-12-09 14:53:04 +0100

    update: update copyright to current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-          <div className='bg-red-500 h-[10vh] px-4 flex items-center justify-between w-full'>
-              <p className='text-center w-full text-white font-semibold'>Hapsie &copy; 2023</p>
-          </div>
+      <div className='bg-red-500 h-[10vh] px-4 flex items-center justify-between w-full'>
+        <p className='text-center w-full text-white font-semibold'>Hapsie &copy; 2024</p>
+      </div>
     </>
   )
 }
